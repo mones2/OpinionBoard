5ee1c746cddce09b77dd22cd5cbc3c2cf0c3dcde
mones2 <monesgharaibeh@gmail.com>
2025-12-15 12:25:43 +0300

    update the ui with use Form Status

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,54 +1,62 @@
-import { useActionState } from "react";
+import { useActionState,use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 export function NewOpinion() {
-  function opinoinAction(prevState, formData) {
+  const {addOpinion} = use(OpinionsContext);
+  async function opinoinAction(prevState, formData) {
     const userName = formData.get('userName');
     const title = formData.get('title');
     const body = formData.get('body');
 
     let errors = [];
-    if (!userName) {
-      errors.push('add a user name');
+    if (userName.length < 4) {
+      errors.push(' user name');
     }
-    if (!title) {
-      errors.push('add a titel');
+    if (title.length < 3) {
+      errors.push(' titel');
     }
-    if (!body) {
-      errors.push('add a body');
+    if (body.length < 4) {
+      errors.push('body');
     }
 
     if (errors.length > 0) {
-      return { errors: errors }
+      return {
+        errors: errors, enteredValues: {
+          title, body, userName
+        },
+      }; 
     }
-    return {errors:null}
+    await addOpinion({title, body, userName}); 
+    return { errors: null }
   }
-  const [formState,fromAction,pending] = useActionState(opinoinAction,{errors:null});
+  const [formState, fromAction, pending] = useActionState(opinoinAction, { errors: null });
 
   return (
-    <form action={fromAction}>
+    
       <div id="new-opinion">
         <h2>Share your opinion!</h2>
-        <form>
+        <form action={fromAction}>
           <div className="control-row">
             <p className="control">
               <label htmlFor="userName">Your Name</label>
-              <input type="text" id="userName" name="userName" />
+              <input type="text" id="userName" name="userName" defaultValue={formState.enteredValues?.userName} />
             </p>
 
             <p className="control">
               <label htmlFor="title">Title</label>
-              <input type="text" id="title" name="title" />
+              <input type="text" id="title" name="title" defaultValue={formState.enteredValues?.title}/>
             </p>
           </div>
           <p className="control">
             <label htmlFor="body">Your Opinion</label>
-            <textarea id="body" name="body" rows={5}></textarea>
+            <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body}></textarea>
           </p>
+          {formState.errors && <ul className="errors">
+            {formState.errors.map(error => <li key={error}>{error}</li>)}
+            </ul>}
 
-          <p className="actions">
-            <button type="submit">Submit</button>
-          </p>
+          <Submit />
         </form>
       </div>
-    </form>
   );
 }
--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,4 +1,16 @@
+import {use} from 'react';
+import { OpinionsContext } from '../store/opinions-context';
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const{upvoteOpinion, downvoteOpinion}  = use(OpinionsContext);
+
+  async function upvoteAction(){
+    upvoteOpinion();
+  }
+
+  async function downvoteAction(){
+    downvoteOpinion();
+  }
   return (
     <article>
       <header>
@@ -7,7 +19,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -27,7 +39,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
